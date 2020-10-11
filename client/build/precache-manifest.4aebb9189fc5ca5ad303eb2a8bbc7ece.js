b3c1390cb1c37642cb9e2226ea1f837b0b98df4d
Jomark Lopez <lopez.jomark@gmail.com>
2020-10-11 18:15:57 +0800

    Changed server so that express will serve up index html file if root is unrecognized

---
+++ client/build/precache-manifest.4aebb9189fc5ca5ad303eb2a8bbc7ece.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f4c70366e1217a14b7f49d04d922644",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15e9601761d31fd195bf",
+    "url": "/static/css/main.124fb802.chunk.css"
+  },
+  {
+    "revision": "43644a64d1da2d2044e4",
+    "url": "/static/js/2.78c5aa23.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.78c5aa23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e9601761d31fd195bf",
+    "url": "/static/js/main.530393ca.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- server.js
+++ server.js
@@ -42,6 +42,17 @@ app.use(express.json())
 app.use(userRouter)
 app.use(quizRouter)
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 // Set server to use port 3001
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${PORT}`);
