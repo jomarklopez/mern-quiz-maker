a50fbf2f28f28faa8f811f76596854c94e06c21c
Jomark Lopez <lopez.jomark@gmail.com>
2020-10-13 06:25:07 +0800

    serve production assets bug fix

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Router, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { createBrowserHistory } from '../history';
+import { createBrowserHistory } from 'history'
 import NavBar from './NavBar';
 import SideBar from './SideBar';
 import Login from './pages/Login';
