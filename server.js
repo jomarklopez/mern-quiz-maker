635785b8d7794f1ce50b94b324f6c51ccabb941c
Jomark Lopez <lopez.jomark@gmail.com>
2020-10-13 13:25:49 +0800

    serve production assets bug fix

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', function (req, res) {
+        console.log('Express sent index.html');
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
     
